c98b7e4a027ef2630f783df3576dce53ce0767a8
mdutt247 <59934065+mdutt247@users.noreply.github.com>
2022-09-02 07:42:57 +0530

    Playbook updated

---
+++ lemp_ubuntu2004/files/dump.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS mditech (           
+  message varchar(255) NOT NULL         
+  ) ENGINE=MyISAM DEFAULT CHARSET=utf8;         
+  INSERT INTO mditech(message) VALUES('Ansible is fun');
+  INSERT INTO mditech(message) VALUES('Get ready');         
+  INSERT INTO mditech(message) VALUES('To play with it');
+  INSERT INTO mditech(message) VALUES('mditech.net@gmail.com');
